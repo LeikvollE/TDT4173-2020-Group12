7892ae496515e496fb1ab8fa56a2a43ebf470e97
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 13:51:52 +0200

    Added normalisation to stars

--- stars.py
+++ stars.py
@@ -2,4 +2,5 @@ import numpy as np
 
 def load_stars():
     stars = np.genfromtxt('data/stars.csv', delimiter=',', skip_header=1)
-    return stars[:,:-3]
+    stars = stars[:,:-3]
+    return (stars - np.min(stars, axis=0)) / (np.max(stars) - np.min(stars))
