9ec3f5089fe111220efaf3cf5341962be3165309
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 15:55:53 +0200

    Visualisations of mnist

--- test.py
+++ test.py
@@ -3,16 +3,24 @@ from kmeans.basic import kmeans
 import mnist
 from stars import load_stars
 from loss.euclidean_loss import euclidean_loss
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def main():
     stars = load_stars()
     c, a = kmeans(stars, 6)
-    print('loss %d' % euclidean_loss(stars, c, a))
+    print(euclidean_loss(stars, c, a))
 
     m = mnist.mnist()
     c, a = kmeans(m.get_test_set(), 10)
-    print('loss %d' % euclidean_loss(m.get_test_set(), c, a))
+    print(euclidean_loss(m.get_test_set(), c, a))
+    for i in range(10):
+        plt.imshow(np.reshape(c[i], (28, 28)))
+        plt.show()
+
+
+
 
 
 if __name__ == '__main__':
