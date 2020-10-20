0f901bcea56433bafa991824099696003462b838
Gaute Ravndal <gaute.ravndal@gmail.com>
2020-10-20 15:31:41 +0200

    fix print statement

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 from kmeans.basic import kmeans
+from loss.euclidean_loss import euclidean_loss
 import mnist
 from stars import load_stars
-from loss.euclidean_loss import euclidean_loss
 
 
 def main():
     stars = load_stars()
     c, a = kmeans(stars, 6)
-    print('loss %d' % euclidean_loss(stars, c, a))
+    print(a)
+    print(euclidean_loss(stars, c, a))
 
     m = mnist.mnist()
     c, a = kmeans(m.get_test_set(), 10)
-    print('loss %d' % euclidean_loss(m.get_test_set(), c, a))
+    print(euclidean_loss(m.get_test_set(), c, a))
 
 
 if __name__ == '__main__':
