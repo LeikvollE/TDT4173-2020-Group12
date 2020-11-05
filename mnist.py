2b181133c211241a47475ac02e964810fb7f16cd
Gaute Ravndal <gaute.ravndal@gmail.com>
2020-11-05 14:33:14 +0100

    mnist: proper minimax scaling

--- mnist.py
+++ mnist.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 import pandas
-import numpy as np
+from sklearn.preprocessing import minmax_scale
 from accuracy import accuracy
 
 
 class mnist():
     def __init__(self):
         self.test_set = pandas.read_csv('data/mnist_test.csv')
-        self.test_set = (self.test_set - np.min(self.test_set)) / (np.max(self.test_set) - np.min(self.test_set) + 0.0001)
         self.training_set = pandas.read_csv('data/mnist_train.csv')
-        self.training_set = (self.training_set - np.min(self.training_set)) / (np.max(self.training_set) - np.min(self.training_set) + 0.0001)
 
     def get_trainig_set(self):
-        return self.training_set.iloc[:, 1:].to_numpy()
+        return minmax_scale(self.training_set.iloc[:, 1:].to_numpy())
 
     def get_test_set(self):
-        return self.test_set.iloc[:, 1:].to_numpy()
+        return minmax_scale(self.test_set.iloc[:, 1:].to_numpy())
 
     def get_accuracy(self, assignments, something='test'):
         if something == 'test':
