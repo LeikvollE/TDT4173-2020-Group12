fade6ff7ff34bc5df05044faa975bdd2e64cdcfe
LM239 <32392200+LM239@users.noreply.github.com>
2020-11-19 13:28:02 +0100

    Update euclidean_loss.py

--- loss/euclidean_loss.py
+++ loss/euclidean_loss.py
@@ -4,5 +4,5 @@ from distance.distance_functions import euclidean_dist
 def euclidean_loss(data, centroids, assignments):
     loss = 0
     for i, entry in enumerate(data):
-        loss += euclidean_dist(entry, centroids[int(assignments[i])])
+        loss += np.power(euclidean_dist(entry, centroids[int(assignments[i])]), 2)
     return loss
