affefc85a19caf9866ccdbc1349170adb08ba14e
Gaute Ravndal <gaute.ravndal@gmail.com>
2020-11-19 14:52:12 +0100

    add manhattan distance

--- loss/euclidean_loss.py
+++ loss/euclidean_loss.py
@@ -1,8 +1,18 @@
 from distance.distance_functions import euclidean_dist
+from distance.distance_functions import manhattan_dist
+import numpy as np
+
+
+def __loss(data, centroids, assignments, d_func):
+    loss = 0
+    for i, entry in enumerate(data):
+        loss += np.power(d_func(entry, centroids[int(assignments[i])]), 2)
+    return loss
+
+
+def manhattan_loss(data, centroids, assignments):
+    return __loss(data, centroids, assignments, manhattan_dist)
 
 
 def euclidean_loss(data, centroids, assignments):
-    loss = 0
-    for i, entry in enumerate(data):
-        loss += euclidean_dist(entry, centroids[int(assignments[i])])
-    return loss
+    return __loss(data, centroids, assignments, euclidean_dist)
