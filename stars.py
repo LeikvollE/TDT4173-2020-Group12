30f54a7e1d7207228b6150a1bcb3ed4144ca20c9
Gaute Ravndal <gaute.ravndal@gmail.com>
2020-11-19 14:45:11 +0100

    add get_accuracy function

--- stars.py
+++ stars.py
@@ -2,6 +2,8 @@ import numpy as np
 from kmeans.basic import kmeans
 from loss.euclidean_loss import euclidean_loss
 import matplotlib.pyplot as plt
+import pandas
+from accuracy import accuracy
 from sklearn.preprocessing import minmax_scale
 
 
@@ -9,6 +11,11 @@ def load_stars():
     stars = np.genfromtxt('data/stars.csv', delimiter=',', skip_header=1)
     stars = stars[:, :-3]
     return minmax_scale(stars)
+
+
+def get_accuracy(assignments):
+    labels = pandas.read_csv('data/stars.csv')[['Star type']].to_numpy().flatten()
+    return accuracy(labels, assignments, allow_miss=True)
 
 
 if __name__ == '__main__':
