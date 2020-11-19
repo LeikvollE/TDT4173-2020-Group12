3913d7dc4da27f2bde3b66880a35bc1cd5cdc847
Gaute Ravndal <gaute.ravndal@gmail.com>
2020-11-19 14:41:10 +0100

    introduce topten and allow_miss parameters

--- accuracy.py
+++ accuracy.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import numpy as np
 import itertools
+# from pprint import pp
 
 
-def accuracy(labels, assignments):
+def accuracy(labels, assignments, topten=False, allow_miss=True):
     count = {}
 
     for l, c in np.c_[labels, assignments]:
@@ -12,6 +13,10 @@ def accuracy(labels, assignments):
         if c not in count[l]:
             count[l][c] = 0
         count[l][c] += 1
+
+    # pp(assignments)
+    # pp(labels)
+    # pp(count)
 
     l = set(labels)
     c = set(assignments)
@@ -23,19 +28,45 @@ def accuracy(labels, assignments):
         zipped = zip(comb, c)
         pairs.append(list(zipped))
 
-    out = (None, 0)
-    for p in pairs:
-        i = 0
-        miss = False
+    if topten:
+        lenght = len(assignments)
+        out = []
+        for p in pairs:
+            i = 0
+            miss = False
 
-        for l, c in p:
-            if c in count[l]:
-                i += count[l][c]
-            else:
-                miss = True
-                break
+            for l, c in p:
+                if c in count[l]:
+                    i += count[l][c]
+                else:
+                    miss = True
+                    break
 
-        if not miss and out[1] <= i:
-            out = (p, i)
+            if not miss:
+                if not out:
+                    out.append((p, i, i / lenght))
+                elif out[-1][1] <= i:
+                    out.append((p, i, i / lenght))
 
-    return out[0], out[1] / len(assignments)
+                if len(out) > 10:
+                    out.pop(0)
+
+        return out
+
+    else:
+        out = (None, 0)
+        for p in pairs:
+            i = 0
+            miss = False
+
+            for l, c in p:
+                if c in count[l]:
+                    i += count[l][c]
+                elif not allow_miss:
+                    miss = True
+                    break
+
+            if not miss and out[1] <= i:
+                out = (p, i)
+
+        return out[0], out[1] / len(assignments)
