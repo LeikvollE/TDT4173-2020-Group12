492e60c38e332facc91cd48e400d1fcd6efa0181
LM239 <32392200+LM239@users.noreply.github.com>
2020-11-22 13:05:56 +0100

    Update basic.py

--- kmeans/basic.py
+++ kmeans/basic.py
@@ -9,7 +9,6 @@ def kmeans(data, k, runs):
     assignments_list = []
     losses = []
     for run in range(runs):
-        print("run:", run)
         centroids = np.random.normal(0.0, 1.0, [k, len(data[0])])
         # centroids = data[random.sample(range(len(data)), k)].copy()
         assignments = -np.ones(len(data))
